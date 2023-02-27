30ce983eda4f65c07588ede6d1c120f1ccb2fb51
b6346 <bhanu.s.pottipareddy@healthpartners.com>
2023-02-27 12:26:31 -0600

    Adding support to pass different browser types

--- src/test/java/com/test/config/BaseModule.java
+++ src/test/java/com/test/config/BaseModule.java
@@ -3,15 +3,25 @@ package com.test.config;
 import com.google.inject.AbstractModule;
 import com.google.inject.Provides;
 import com.test.page.interfaces.LoginPage;
+import lombok.extern.slf4j.Slf4j;
 import org.openqa.selenium.WebDriver;
 
+@Slf4j
 public abstract class BaseModule extends AbstractModule {
 
     @Provides
     WebDriver provideWebdriver() {
+        log.info("Creating driver for {} browser", getBrowser());
         return null;
 
     }
     abstract LoginPage provideLoginPage();
+
+    protected String getBrowser(){
+        String type = System.getProperty("browser") ;
+        type = type == null ? "chrome": type;
+
+        return type;
+    }
 
 }
